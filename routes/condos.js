f0a2527bf370065b44459df1973d2cf5b1a47b63
Garymarena <garym.arena@gmail.com>
2023-10-09 19:17:39 -0500

    Fet/CondosCreateInsomnia

--- routes/condos.js
+++ routes/condos.js
@@ -1,5 +1,25 @@
 import express from "express";
+import Condos from "../models/Condos.js";
 
 const router = express.Router();
+
+//Create
+router.post("/", async (req, res) => {
+  const newCondo = new Condos(req.body);
+
+  try {
+    const saveCondo = await newCondo.save();
+    res.status(200).json(savedCondo);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+//Update
+
+//Delete
+
+//Get
+
+//Get All
 
 export default router;
--- routes/rooms.js
+++ routes/rooms.js
@@ -2,4 +2,14 @@ import express from "express";
 
 const router = express.Router();
 
+//Create
+
+//Update
+
+//Delete
+
+//Get
+
+//Get All
+
 export default router;
