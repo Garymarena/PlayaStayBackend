168d0b28b4e031069b9654e3fb26cd85db550f2c
Garymarena <garym.arena@gmail.com>
2023-10-12 16:36:58 -0500

    Fix-UpdateCondo

--- routes/condos.js
+++ routes/condos.js
@@ -15,7 +15,17 @@ router.post("/", async (req, res) => {
   }
 });
 //Update
-
+router.put("/:id", async (req, res) => {
+  const updateCondo = Condos(req.body);
+  try {
+    const updateCondo = await Condo.findByIdAndUpdate(req.params.id, {
+      $set: req.body,
+    });
+    res.status(200).json(updateCondo);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 //Delete
 
 //Get
